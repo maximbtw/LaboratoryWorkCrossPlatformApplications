ecdb9e9831c5a25b2e7b9634e4267c4e2c01e8e8
maximbtw <traglodet2001@gmail.com>
2021-03-23 14:55:39 +0300

    Добавил лабораторную работу №7

---
+++ src/Lab7/Crawler.java
@@ -0,0 +1,62 @@
+package Lab7;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Crawler {
+
+    private HashMap<String, URLDepthPair> visited;
+    private LinkedList<URLDepthPair> notVisited;
+    private int maxDepth = 0;
+
+    public Crawler(String URL, int maxDepth){
+        visited = new HashMap<>();
+        notVisited = new LinkedList<>();
+        notVisited.add(new URLDepthPair(URL,0));
+        this.maxDepth = maxDepth;
+    }
+
+    public void run() {
+        while(notVisited.size() > 0) {
+            URLDepthPair link = notVisited.pop();
+            if(visited.containsKey(link.getURL())) continue;
+            visited.put(link.getURL(), link);
+            System.out.println(link);
+            if(link.getDepth() != maxDepth)
+                findLinks(link);
+        }
+
+        System.out.println();
+        System.out.printf("Всего ссылок: " + visited.size());
+    }
+
+    private void findLinks(URLDepthPair link){
+        try {
+            URL url = new URL(link.getURL());
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            Scanner scanner = new Scanner(connection.getInputStream());
+            System.out.println(scanner.next());
+
+            while (scanner.findWithinHorizon("<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1", 0) != null){
+                String newURL = scanner.match().group(2);
+                createNewLink(newURL, link);
+            }
+        }
+        catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private void createNewLink(String newURL, URLDepthPair link){
+        if (newURL.startsWith("/")) {
+            newURL = link.getURL() + newURL;
+        }
+        else if (!newURL.startsWith("http")) return;
+        URLDepthPair newLink = new URLDepthPair(newURL, link.getDepth() + 1);
+        notVisited.add(newLink);
+    }
+}
---
+++ src/Lab7/Main.java
@@ -0,0 +1,8 @@
+package Lab7;
+
+public class Main {
+    public static void main(String[] args) {
+        Crawler crawler = new Crawler("https://vk.com/im?sel=c39",2);
+        crawler.run();
+    }
+}
